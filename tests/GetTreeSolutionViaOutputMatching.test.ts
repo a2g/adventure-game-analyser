1a2e39878df146dbd85052fad7918e70703c4af3
Anthony Cassidy <anthony.cassidy@sap.com>
2021-05-04 23:13:05 +1200

    Now tests can be debugged

--- tests/GetTreeSolutionViaOutputMatching.test.ts
+++ tests/GetTreeSolutionViaOutputMatching.test.ts
@@ -1,19 +1,11 @@
 //Typescript Unit test
 import assert = require('assert');
-import { TruthTable } from "../TruthTable";
 import { GetTreeSolutionViaOutputMatching } from '../GetTreeSolutionViaOutputMatching';
 import { Transaction } from '../Transaction';
 import { GetMapFromJSonGlossy } from '../GetMapFromJSonGlossy';
 
 
-function AddToMap(map: Map<string, Transaction[]>, t: Transaction) {
-    // initiatize array, if it hasn't yet been
-    if (!map.has(t.output)) {
-        map.set(t.output, new Array<Transaction>());
-    }
-    // always add to list
-    map.get(t.output)?.push(t);
-}
+ 
 
 describe("GetTreeSolutionViaOutputMatching", () => {
     it("CreateMapAndTest", () => {
@@ -26,6 +18,6 @@ describe("GetTreeSolutionViaOutputMatching", () => {
         const collection = collections.array[0];
         const leafNodes = collection.GetLeafNodes();
         assert.equal(4, leafNodes.length);
-        
+
     })
 })
