880cd3e43514fc78a6788ed497d99c1f6109e917
Anthony Cassidy <anthony.cassidy@sap.com>
2021-05-06 00:35:17 +1200

    added

--- tests/GetTreeSolutionViaOutputMatching.test.ts
+++ tests/GetTreeSolutionViaOutputMatching.test.ts
@@ -13,11 +13,11 @@ describe("GetTreeSolutionViaOutputMatching", () => {
         const map = GetMapFromJSonGlossy();
         const collections = GetTreeSolutionViaOutputMatching(map, "prop-death-by-guitar");
 
-        assert.equal(1, collections.array.length);
+        assert.strictEqual(1, collections.array.length);
 
         const collection = collections.array[0];
         const leafNodes = collection.GetLeafNodes();
-        assert.equal(4, leafNodes.length);
+        assert.strictEqual(4, leafNodes.length);
 
     })
 })
