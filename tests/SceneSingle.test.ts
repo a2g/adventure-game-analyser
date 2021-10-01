1da4a07a1a411dd9107872c24f7a0bcf824f36f9
Anthony Cassidy <githuba2g@antcassidy.com>
2021-10-02 01:35:47 +1300

    Added test for SingleScene.ts

---
+++ tests/SceneSingle.test.ts
@@ -0,0 +1,29 @@
+//Typescript Unit test
+import assert = require('assert');
+import { SolverViaRootNode } from '../src/SolverViaRootNode';
+import { SolutionNode } from '../src/SolutionNode';
+import { Solution } from '../src/Solution';
+import { SceneSingle } from '../src/SceneSingle';
+
+
+describe("SceneSingle", () => {
+  
+    it("Test GetMapOfAllStartingThings", () => {
+        const scene = new SceneSingle("./src/TestHighPermutationSolutionScene.json");
+       
+        // this failed recently
+        const map = scene.GetMapOfAllStartingThings();
+       // assert.strictEqual(collection.length, 1);
+        assert.ok(true);
+        assert.strictEqual(map.size,14);
+        assert.ok(map.has("inv_shared_toy"));
+        const set = map.get("inv_shared_toy");
+        if(set){
+            assert.strictEqual(set.size, 4);
+            assert.ok(set.has("char1"));
+            assert.ok(set.has("char2"));
+            assert.ok(set.has("char3"));
+            assert.ok(set.has("char4"));
+        }
+    });
+})
